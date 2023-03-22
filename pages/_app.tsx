95ea0362ab7efc6c7f5cbfb346dcde24fad6cbee
JohnnyQu4037 <85147657+JohnnyQu4037@users.noreply.github.com>
2023-03-22 15:55:55 +1100

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import 'antd/dist/reset.css';
 import AppLayout from '../components/Layout';
 import { Spin } from 'antd';
 import type { AppProps } from 'next/app'
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps,router }: AppProps) {
   if (router.pathname.startsWith('/dashboard')) {
